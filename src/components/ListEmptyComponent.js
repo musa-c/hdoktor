d8f00b00277a53e5f4e8bda696d267157ba9b8b0
Musa Civelek <musacivelekx@gmail.com>
2022-07-30 22:44:26 +0300

    refreshing kontrolü ile koşullu renderlama işlemi sağlandı.

--- src/components/ListEmptyComponent.js
+++ src/components/ListEmptyComponent.js
@@ -1,13 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const ListEmptyComponent = ({text}) => {
+const ListEmptyComponent = ({text, refreshing}) => {
   return (
-    <View style={{ flex:1, justifyContent:"center", alignItems:"center"}}>
-    <Text style={{fontSize:20, color:"grey"}}>
-      {text}
-    </Text>
-  </View>
+    <>
+      {!refreshing &&
+        <View style={{ flex:1, justifyContent:"center", alignItems:"center"}}>
+        <Text style={{fontSize:20, color:"grey"}}>
+          {text}
+        </Text>
+      </View> }
+      </>
+      
+      
   )
 }
 
