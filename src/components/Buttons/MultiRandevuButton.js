5dee03f9e7079e210b613d19c93fde7b21e4515d
Musa Civelek <musacivelekx@gmail.com>
2022-08-04 03:20:24 +0300

    çoklu randevuda görülecek buton oluşturuldu.

---
+++ src/components/Buttons/MultiRandevuButton.js
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MultiRandevuButton = ({randevuCount}) => {
+  return (
+    <View style={{flexDirection:"row", backgroundColor:"#FF2442" ,alignSelf:"baseline", borderRadius:10, paddingHorizontal:10, paddingVertical:5, marginStart:5}}>
+   
+        <View style={{backgroundColor:"#3DB2FF", justifyContent:"center", alignItems:"center", width:22, height:22, borderRadius:30}}>
+<Text style={{color:"white", fontWeight:"bold"}}>{randevuCount}</Text>
+</View>
+
+    <Text mode="contained" 
+    style={{
+        fontSize:22,
+        color:"white",
+        fontWeight:"bold"
+    }}>  Adet Randevu
+          </Text>
+          </View>
+  )
+}
+
+export default MultiRandevuButton
