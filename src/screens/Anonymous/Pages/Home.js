39e05cc0de3aa0d8f36342f1354da32bce2160ae
Musa Civelek <musacivelekx@gmail.com>
2022-09-07 03:07:41 +0300

    limit redner and reting  orderBy add

--- src/screens/Anonymous/Pages/Home.js
+++ src/screens/Anonymous/Pages/Home.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, memo } from "react";
 import {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
   FlatList,
+  ActivityIndicator,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { useNavigation } from "@react-navigation/native";
@@ -14,11 +15,18 @@ import firebase from "firebase/compat/app";
 import { Avatar } from "react-native-elements";
 import Separator from "../../../components/Separator";
 import ListEmptyComponent from "../../../components/ListEmptyComponent";
+import { isEqual } from "lodash";
+import RenderItem from "./RenderItem";
+import { useCallback } from "react";
 
 function Home({ route }) {
   const [users, setUsers] = useState([]);
   const [refresh, setRefresh] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
+  const [LastCount, setLastCount] = useState();
+  const [loading, setLoading] = useState(false);
+  const [LoadingDone, setLoadingDone] = useState(false);
+
   const w_anonymous = route.params.w_anonymous;
 
   useEffect(() => {
@@ -26,26 +34,200 @@ function Home({ route }) {
     if (!unmounted) {
       setRefreshing(true);
     }
-    firebase
+    var first = firebase
       .firestore()
       .collection("D_user")
-      .onSnapshot((querySnapshot) => {
-        const users = [];
-        querySnapshot.forEach((documentSnapshot) => {
-          users.push({
-            ...documentSnapshot.data(),
-            key: documentSnapshot.id,
-          });
+      .orderBy("rating", "desc")
+      .limit(5);
+
+    first.get().then((querySnapshot) => {
+      const users = [];
+      querySnapshot.forEach((documentSnapshot) => {
+        users.push({
+          ...documentSnapshot.data(),
+          key: documentSnapshot.id,
         });
-        if (!unmounted) {
-          setUsers(users);
-          setRefreshing(false);
-        }
       });
+      if (!unmounted) {
+        setLastCount(querySnapshot.docs[querySnapshot.docs.length - 1]);
+        setUsers(users);
+        setRefreshing(false);
+      }
+    });
+    // firebase
+    //   .firestore()
+    //   .collection("D_user")
+    //   .onSnapshot((querySnapshot) => {
+    //     const users = [];
+    //     querySnapshot.forEach((documentSnapshot) => {
+    //       users.push({
+    //         ...documentSnapshot.data(),
+    //         key: documentSnapshot.id,
+    //       });
+    //     });
+    //     if (!unmounted) {
+    //       setUsers(users);
+    //       setRefreshing(false);
+    //     }
+    //   });
     return () => {
       unmounted = true;
     };
   }, [refresh]);
+
+  const getOtherData = () => {
+    setLoading(true);
+    if (LastCount != undefined) {
+      firebase
+        .firestore()
+        .collection("D_user")
+        .orderBy("rating", "desc")
+        .startAfter(LastCount)
+        .limit(5)
+        .get()
+        .then((querySnapshot) => {
+          if (
+            isEqual(
+              querySnapshot.docs[querySnapshot.docs.length - 1],
+              LastCount
+            )
+          ) {
+            // eşit ise
+            setLoadingDone(true);
+            setLoading(false);
+          } else {
+            // eşit değil ise
+            const usersOther = [];
+            querySnapshot.forEach((documentSnapshot) => {
+              usersOther.push({
+                ...documentSnapshot.data(),
+                key: documentSnapshot.id,
+              });
+            });
+            setUsers(users.concat(usersOther));
+            setLastCount(querySnapshot.docs[querySnapshot.docs.length - 1]);
+            setLoading(false);
+          }
+        })
+        .catch((e) => {
+          const erroCorde = e.code;
+          setLoading(false);
+          console.log(erroCorde);
+          console.log(e);
+          console.log("hata");
+        });
+    } else {
+      setLoading(false);
+    }
+  };
+
+  //console.log("lastoutn:", LastCount);
+  const footerIndicator = () => {
+    return loading ? (
+      <View
+        style={{
+          paddingVertical: 20,
+        }}
+      >
+        <ActivityIndicator animating size="small" />
+      </View>
+    ) : null;
+  };
+
+  // const renderItem = ({ item }) => (
+  //   <View style={{ flex: 1, backgroundColor: "red" }}>
+  //     <Text>abc</Text>
+  //     <TouchableOpacity
+  //       onPress={() =>
+  //         navigation.navigate("MoreDoctorInfo", {
+  //           doctorId: item.key,
+  //           name: item.name,
+  //           brans: item.brans,
+  //           gender: item.cinsiyet,
+  //           email: item.email,
+  //           time1: item.time1.toDate(),
+  //           time2: item.time2.toDate(),
+  //           CalisilanYer: item.CalisilanYer,
+  //           avatar: item.avatar,
+  //           rating: item.rating,
+  //         })
+  //       }
+  //     >
+  //       <View style={styles.card}>
+  //         <View style={styles.cardImage}>
+  //           {item.avatar == "" ? (
+  //             <Avatar
+  //               size={95}
+  //               source={require("../../../rec/Avatars/DefaultDoctorAvatar.png")}
+  //               avatarStyle={styles.imageStyle}
+  //               activeOpacity={0.7}
+  //               rounded
+  //             />
+  //           ) : (
+  //             <Avatar
+  //               size={95}
+  //               // rounded
+  //               source={{ uri: item.avatar }}
+  //               avatarStyle={styles.imageStyle}
+  //               activeOpacity={0.7}
+  //               rounded
+  //             />
+  //           )}
+  //         </View>
+  //         <View style={styles.CardInfo}>
+  //           <Text
+  //             style={{
+  //               color: "black",
+  //               fontSize: 19,
+  //               paddingStart: 5,
+  //               fontWeight: "bold",
+  //             }}
+  //           >
+  //             {item.name}
+  //           </Text>
+  //           <Text
+  //             style={{
+  //               color: "black",
+  //               fontSize: 17,
+  //               paddingStart: 5,
+  //               paddingBottom: 5,
+  //               textAlign: "auto",
+  //             }}
+  //           >
+  //             <Icon name="stethoscope" size={22} color="#B71C1C" />
+  //             &nbsp;&nbsp;{item.brans}
+  //           </Text>
+
+  //           {item.CalisilanYer != "" ? (
+  //             <Text style={{ color: "black", fontSize: 17, paddingStart: 5 }}>
+  //               <Icon name="h-square" size={22} color="#B71C1C" />
+  //               &nbsp;&nbsp;{item.CalisilanYer}
+  //             </Text>
+  //           ) : null}
+
+  //           <View
+  //             style={{
+  //               marginHorizontal: 5,
+  //               marginTop: 5,
+  //               alignItems: "flex-end",
+  //             }}
+  //           >
+  //             <Text
+  //               style={{
+  //                 color: "grey",
+  //                 fontSize: 15,
+  //                 fontStyle: "italic",
+  //               }}
+  //             >
+  //               Daha fazla bilgi al
+  //             </Text>
+  //           </View>
+  //         </View>
+  //       </View>
+  //     </TouchableOpacity>
+  //     <Separator />
+  //   </View>
+  // );
 
   const navigation = useNavigation();
   return (
@@ -64,7 +246,13 @@ function Home({ route }) {
       <Ad />
       <FlatList
         contentContainerStyle={{ flexGrow: 1 }}
+        onEndReached={() => {
+          if (!LoadingDone) {
+            getOtherData();
+          }
+        }}
         refreshing={refreshing}
+        ListFooterComponent={footerIndicator}
         onRefresh={() => {
           setRefresh(!refresh);
           setRefreshing(true);
@@ -76,105 +264,13 @@ function Home({ route }) {
           />
         }
         data={users}
-        renderItem={(element) => (
-          <View style={styles.DenemeCont}>
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate("MoreDoctorInfo", {
-                  doctorId: element.item.key,
-                  name: element.item.name,
-                  brans: element.item.brans,
-                  gender: element.item.cinsiyet,
-                  email: element.item.email,
-                  time1: element.item.time1.toDate(),
-                  time2: element.item.time2.toDate(),
-                  CalisilanYer: element.item.CalisilanYer,
-                  avatar: element.item.avatar,
-                  rating: element.item.rating,
-                })
-              }
-            >
-              <View style={styles.card}>
-                <View style={styles.cardImage}>
-                  {element.item.avatar == "" ? (
-                    <Avatar
-                      size={95}
-                      source={require("../../../rec/Avatars/DefaultDoctorAvatar.png")}
-                      avatarStyle={styles.imageStyle}
-                      activeOpacity={0.7}
-                      rounded
-                    />
-                  ) : (
-                    <Avatar
-                      size={95}
-                      // rounded
-                      source={{ uri: element.item.avatar }}
-                      avatarStyle={styles.imageStyle}
-                      activeOpacity={0.7}
-                      rounded
-                    />
-                  )}
-                </View>
-                <View style={styles.CardInfo}>
-                  <Text
-                    style={{
-                      color: "black",
-                      fontSize: 19,
-                      paddingStart: 5,
-                      fontWeight: "bold",
-                    }}
-                  >
-                    {element.item.name}
-                  </Text>
-                  <Text
-                    style={{
-                      color: "black",
-                      fontSize: 17,
-                      paddingStart: 5,
-                      paddingBottom: 5,
-                      textAlign: "auto",
-                    }}
-                  >
-                    <Icon name="stethoscope" size={22} color="#B71C1C" />
-                    &nbsp;&nbsp;{element.item.brans}
-                  </Text>
-
-                  {element.item.CalisilanYer != "" ? (
-                    <Text
-                      style={{ color: "black", fontSize: 17, paddingStart: 5 }}
-                    >
-                      <Icon name="h-square" size={22} color="#B71C1C" />
-                      &nbsp;&nbsp;{element.item.CalisilanYer}
-                    </Text>
-                  ) : null}
-
-                  <View
-                    style={{
-                      marginHorizontal: 5,
-                      marginTop: 5,
-                      alignItems: "flex-end",
-                    }}
-                  >
-                    <Text
-                      style={{
-                        color: "grey",
-                        fontSize: 15,
-                        fontStyle: "italic",
-                      }}
-                    >
-                      Daha fazla bilgi al
-                    </Text>
-                  </View>
-                </View>
-              </View>
-            </TouchableOpacity>
-            <Separator />
-          </View>
-        )}
+        renderItem={useCallback(_renderItem, [users])}
       />
     </View>
   );
 }
+
+const _renderItem = ({ item }) => <RenderItem item={item} />;
 
 const styles = StyleSheet.create({
   card: {
